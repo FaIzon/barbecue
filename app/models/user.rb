cc30002680dd4a2befb9fe25e94191d5d0a87496
FaIzon <dimer90@gmail.com>
2017-03-19 18:08:37 +0700

    Add relation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :events
+
+  validates :name, presence: true, length: { maximum: 35 }
+  validates :email, presence: true, length: { maximum: 100 }
+  validates :email, uniqueness: true
+  validates_format_of :email, with: /@/
 end
